fa4a7d385b76e7df766473812e71d0f5649d4cb3
Darin Kadrioski <darin@nebiru.com>
2017-06-19 21:24:26 -0700

    general cleanup

---
+++ MIDIUSB_1x1/Leds.h
@@ -0,0 +1,80 @@
+#pragma once
+//------------------------------------------------------------------------------
+// Generic led pin and level mapping, defines LED, LED_OFF, LED_ON
+// Author: Fabien 
+// License: GPL V3 © 2016 Fabien (https://github.com/fab672000)
+ //-----------------------------------------------------------------------------
+#if defined(ARDUINO_SAM_DUE)
+//#pragma message("Due")
+  const byte LED = 13;
+#elif defined(STM32_MCU_SERIES)
+//#pragma message("STM32 MCU")
+  const byte LED = PC13;
+#elif defined(ARDUINO_AVR_MICRO)
+//#pragma message("Micro")
+  const byte LED = 17; // TX led on micro
+#elif defined(ARDUINO_AVR_PRO) || defined(ARDUINO_AVR_MINI)
+//#pragma message("Mini")
+  const byte LED = 13; // TX led on micro
+#elif defined(ARDUINO_AVR_LEONARDO)
+//#pragma message("Leonardo")
+  const byte LED = 13; // TX led on micro
+#elif defined(ESP8266_ESP12)
+//#pragma message("NodeMCU 1.0")
+  const byte LED = 13; // TX led on micro
+#else 
+//#pragma message("Default")
+  const byte LED = 17;
+#endif
+#if defined(STM32_MCU_SERIES) || defined(ARDUINO_AVR_MICRO)
+# define LED_ON   LOW
+# define LED_OFF HIGH
+#else
+# define LED_OFF  LOW
+# define LED_ON  HIGH
+#endif  
+
+const byte  led1 = LED;
+const byte  led2 = TXLED1;
+
+class Leds
+{
+ public:
+  
+  static void Setup()
+  {
+    pinMode(led1, OUTPUT);
+    pinMode(led2, OUTPUT);
+  }
+  
+  static void Blink(byte led, int del=500)
+  {
+    digitalWrite(led, LED_ON);  
+    delay(del);
+    digitalWrite(led, LED_OFF); 
+    delay(del);
+  }
+  
+  static void BlinkAlternateLeds( int del=150)
+  {
+     delay(150);
+     digitalWrite(led1, LED_ON);
+     TXLED1;
+     delay(150);
+     digitalWrite(led1, LED_OFF);
+     TXLED0;
+  }
+  
+  static void AllLedsOff()
+  {
+      digitalWrite(led1, LED_OFF);
+      TXLED1;
+  }
+
+  static void AliveSignal(int del = 150)
+  {
+    for (uint8_t i=0; i< 2; i++) BlinkAlternateLeds();
+    AllLedsOff();
+  }
+};
+
---
+++ MIDIUSB_1x1/README.md
@@ -0,0 +1,6 @@
+# MIDIUSB_1x1
+MIDI Interface 1 in 1 out based on Arduino MidiUSB (https://github.com/arduino-libraries/MIDIUSB) and Midi (https://github.com/FortySevenEffects/arduino_midi_library) libraries
+
+Works with iPad and ios native midi drivers (must patch MidiUSB power settings as instructed in the .ino file)
+![alt midi-hardware](HardwareExample.jpg)
+License: GPL V3 © 2016 Fabien (https://github.com/fab672000)
